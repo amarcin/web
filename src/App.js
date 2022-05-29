0af53d92e566c0cc14bf4a21a6221ffdf642555a
Augustin James Marcin <amarcin@Mac-mini.attlocal.net>
2022-05-29 16:06:29 -0700

    hello world reset

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import './App.css';
-import {useState, useEffect} from "react";
 
-const blogQuery = `
-{
-  blogPostCollection {
-    items {
-      title
-      author
-      postDate
-      article {json}
-    }
-  }
-}
-`
 
 function App() {
-  // define the initial state
-  const [page, setPage] = useState(null);
-
-  // show a loading screen case the data hasn't arrived yet
-  if (!page) {
-    return "Loading...";
-  }
-
-  // return statement and JSX template.
-  // ...
-
   return (
-    <h1>
-      Homepage
-    </h1>
-  );
+    <div className="App">
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
 }
 
-export default App;
+export default App;  
