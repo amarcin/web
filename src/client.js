6e0c48db45063c0571de4990c23132c28e9f1118
Augustin James Marcin <amarcin@Mac-mini.attlocal.net>
2022-05-31 22:43:51 -0700

    created security issue to make compatible with web fetch

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import * as contentful from 'contentful'
 
+//commenting out references until issue is resolved (SECURITY ISSUE)
 export const client = contentful.createClient({
-  space: process.env.REACT_APP_SPACE_ID,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: 'n44ys8eqz51t', //process.env.REACT_APP_SPACE_ID,
+  accessToken: '4ICMczM0r6vHVuWYBjvBDgTMRytqHih6HJKnXAt73J8' //process.env.REACT_APP_ACCESS_TOKEN
 })
