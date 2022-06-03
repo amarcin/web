e5fb690810f46d47a0b6aafb5b35de9a7e238423
Augustin James Marcin <amarcin@Mac-mini.attlocal.net>
2022-06-02 17:50:42 -0700

    updated background to aws s3 sourcing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Carousel from "./components/Carousel/Carousel";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Navbar from "./components/Navbar/Navbar";
-import videoBg from './VideoBgTrim.mp4'
 
 function App() {
   return (
     <div className="App">
-      <video src={videoBg} autoPlay loop muted />
+      <video src={'https://amplify-web-staging-11952-deployment.s3.us-west-1.amazonaws.com/VideoBgTrim.mp4'} autoPlay loop muted />
       <Navbar />
       <HeroSection />
       <Carousel />
