571abea516c9391848284a31249cf9915ec9845b
Augustin James Marcin <amarcin@Mac-mini.attlocal.net>
2022-06-04 16:44:10 -0700

    links open in new tab, added security feature for older browsers

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,29 +5,27 @@ function HeroSection() {
     <div className="hero-container">
       <h1>Augustin James Marcin</h1>
       <p className="hero-links">
-        <a className="social-link" href="http://linkedin.com/in/augustinmarcin">
+        <a className="social-link" href="http://linkedin.com/in/augustinmarcin" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-linkedin-in"></i>
         </a>
         &nbsp;&nbsp;&nbsp;
-        <a className="social-link" href="https://github.com/amarcin">
+        <a className="social-link" href="https://github.com/amarcin" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-github-alt"></i>
         </a>
         &nbsp;&nbsp;&nbsp;
         <a
-          className="social-link"
-          href="https://www.instagram.com/augustinmarcin/"
+          className="social-link" href="https://www.instagram.com/augustinmarcin/" target="_blank" rel="noopener noreferrer"
         >
           <i className="fa-brands fa-instagram"></i>
         </a>
         &nbsp;&nbsp;&nbsp;
         <a
-          className="social-link"
-          href="https://youtube.com/channel/UCd14Jj1HKAUFUZckWwSjZ3A"
+          className="social-link" href="https://youtube.com/channel/UCd14Jj1HKAUFUZckWwSjZ3A" target="_blank" rel="noopener noreferrer"
         >
           <i className="fa-brands fa-youtube"></i>
         </a>
         &nbsp;&nbsp;&nbsp;
-        <a className="social-link" href="https://twitter.com/AugustinMarcin">
+        <a className="social-link" href="https://twitter.com/AugustinMarcin" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-twitter"></i>
         </a>
       </p>
